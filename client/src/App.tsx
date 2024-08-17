7fca938250ecf53675ac8635f0bc205156cf4a72
Samwise-B <sam.burcher99@gmail.com>
2024-08-17 15:43:17 +0100

    updated pw bank prototype, added add pass screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,30 +24,66 @@ function Navigation() {
   )
 }
 
+type PassBankItemProps = {
+  site_favicon: string;
+  siteURL: string;
+  email: string;
+  username: string;
+}
+
+function PassBankItem({site_favicon, siteURL, email, username}: PassBankItemProps) {
+  return (
+    <button type="button" className="list-group-item list-group-item-action rounded-0" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-current="true">
+      <div className="row">
+        <div className="col-sm-1 fw-bold">
+          <img src={site_favicon} alt="" className="rounded"></img>
+        </div>
+        <div className='col-sm-11'>
+          <div className='row mx-auto justify-content-center'>
+            {siteURL}
+          </div>
+          <div className='row justify-content-center'>
+            {email}
+          </div>
+        </div>
+      </div>
+    </button>
+)
+}
+
 function PassBank() {
   return (
     <div className='container-fluid'>
-      <ul className="list-group">
-        <li className="list-group-item">
-        <div className="row">
-          <div className="col-sm-1 fw-bold">favicon</div>
-          <div className='col-sm-11'>
-            <div className='row mx-auto justify-content-center'>
-              www.placeholder.com
-            </div>
-            <div className='row justify-content-center'>
-              placeholder@gmail.com
-            </div>
-          
-          </div>
-          
+      <div className='list-group'>
+        <PassBankItem site_favicon='public/vite.svg' siteURL='www.placeholder.com' email='placeholder@email.com' username=''/>
+        <PassBankItem site_favicon='public/vite.svg' siteURL='www.placeholder.com' email='placeholder@email.com' username=''/>
+      </div>
+    </div>
+  )
+}
+
+function PassDetails() {
+  return (
+    <div className="offcanvas offcanvas-end" tabIndex={-1} id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+        <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div className="offcanvas-body">
+        <div>
+          Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
         </div>
-        </li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
-      </ul>
+        <div className="dropdown mt-3">
+          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
+            Dropdown button
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
@@ -58,6 +94,7 @@ function App() {
       <div className='App'>
         <Navigation/>
         <PassBank/>
+        <PassDetails/>
       </div>
     </>
   )
