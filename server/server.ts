7b452b38fc8f88e0adc16b6ae9660b5403727b55
Samwise-B <sam.burcher99@gmail.com>
2024-09-28 01:39:59 +0100

    added backend template for insert, updating and deleting passwords

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,5 @@
-import express, {Express, Request, Response} from "express";
+import express, {Express, Request, response, Response} from "express";
+import { Pool } from "pg";
 import dotenv from "dotenv";
 
 // add .env configuration
@@ -7,8 +8,48 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 5000; // You can choose any port
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from the Node.js backend!');
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+})
+
+app.get('/getPasswords', async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query("SELECT * FROM passwords ORDER BY id ASC");
+    const passwords = result.rows;
+    return res.json(passwords);
+  } catch (err) {
+    throw err;
+  }
+});
+
+app.get('/addPassword', async (req: Request, res: Response) => {
+  const {
+    username,
+    email,
+    password,
+    url,
+  } = req.body;
+
+  try {
+    const insertPassword = "INSERT INTO passwords (username, email, password, url) VALUES ($1, $2, $3, $4) RETURNING *";
+    const passwordList = await pool.query(insertPassword, [username, email, password, url]);
+
+    return res.json(passwordList);
+  } catch (err) {
+    throw err;
+  }
+})
+
+app.get("/updatePassword", async (req: Request, res: Response) => {
+  return "TODO";
+});
+
+app.get("/deletePassword", async (req: Request, res: Response) => {
+  return "TODO";
 });
 
 app.listen(port, () => {
