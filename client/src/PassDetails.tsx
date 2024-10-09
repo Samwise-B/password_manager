ecf49da1c4bf5a0565e2548003f672834f1945d5
Samwise-B <sam.burcher99@gmail.com>
2024-10-09 20:01:52 +0100

    change validation to require username or password

--- client/src/PassDetails.tsx
+++ client/src/PassDetails.tsx
@@ -1,7 +1,7 @@
 import {passDetailProps, IEditButtonProps, PasswordListItem} from './types';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { useState } from 'react';
-import { useForm } from "react-hook-form"
+import { get, useForm, useFormContext, FormProvider } from "react-hook-form"
 import { ErrorMessage } from "@hookform/error-message";
 import { useDeletePassword, useUpdatePassword } from './utils/addPassword';
 import { ErrorBoundary } from "react-error-boundary";
@@ -14,13 +14,21 @@ export function PassDetails({passList, currentIndex, updatePassList, handleClose
     const [passInputType, setPassInputType] = useState<string>("password");
     const [isEditing, setIsEditing] = useState<boolean>(false);
     const [passItem, setPassItem] = useState<PasswordListItem>(passList[currentIndex]);
+    const [staticPassItem, setStaticPassItem] = useState<PasswordListItem>(passList[currentIndex]);
     const [showPassword, setShowPassword] = useState<string>("Show");
+    const [hasChanged, setHasChanged] = useState<boolean>(false);
     const user = useAuth();
-    const methods = useForm({ criteriaMode: "all" });
+    const methods = useForm({ 
+      defaultValues: {
+        ...passItem
+      },
+      criteriaMode: "all" 
+    });
+
     //const passItem = passList[currentIndex];
     //console.log(passItem);
   
-    function handleShowPassword() {
+    function toggleShowPassword() {
       if (passInputType == "password") {
         setPassInputType("text");
         setShowPassword("Hide");
@@ -32,37 +40,55 @@ export function PassDetails({passList, currentIndex, updatePassList, handleClose
   
     function toggleEditPassword() {
       setIsEditing(!isEditing);
-      handleShowPassword();
+      if (!isEditing && passInputType != "text") {
+        toggleShowPassword();
+      }
+      if (isEditing && passInputType != "password") {
+        toggleShowPassword();
+      }
+      setPassItem(staticPassItem);
     }
   
     function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+      if (!hasChanged) {
+        setHasChanged(true);
+      } 
       const {id, value} = e.target;
       setPassItem((prevItem) => ({
         ...prevItem,
         [id]:value
       }));
-      console.log(passItem)
     }
 
     const updatePassword = methods.handleSubmit(async data => {
       console.log("form data:", data);
-      const updatedPassword = await useUpdatePassword({
-        id: passItem.id,
-        site_favicon: "/vite.svg",
-        username: data.username,
-        email: data.email,
-        password: data.password,
-        url: data.url,
-        jwt: user.jwt,
-        handleError: handleError
-      });
-      if (updatedPassword) {
-        updatePassList(updatedPassword, "update");
-      } else {
-        throw new Error(`Error: Password item not found.`);
+      if (hasChanged) {
+        const updatedPassword = await useUpdatePassword({
+          id: passItem.id,
+          site_favicon: "/vite.svg",
+          username: data.username,
+          email: data.email,
+          password: data.password,
+          url: data.url,
+          jwt: user.jwt,
+          handleError: handleError
+        });
+        if (updatedPassword) {
+          updatePassList(updatedPassword, "update");
+          setStaticPassItem(updatedPassword);
+        } else {
+          throw new Error(`Error: Password item not found.`);
+        }
       }
       setIsEditing(false);
     })
+
+    function validateUsernameAndEmail() {
+      if (methods.getValues("email") || methods.getValues("username")) {
+        return true;
+      }
+      return "You must enter an email or username";
+    }
   
     return (
       <>
@@ -76,133 +102,180 @@ export function PassDetails({passList, currentIndex, updatePassList, handleClose
                 errorMessageShort="Failed to Update Password"
                 errorMessageFull='Something went wrong when updating the password.'/>
             }>
+            <FormProvider {...methods}>
             <form onSubmit={e => e.preventDefault()} className="container" noValidate>
               <input type="hidden" name="id" value={passItem.id}></input>
-              <div className="container mb-3">
-                <label htmlFor="email" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="email" 
-                  value={passItem.email} 
-                  readOnly={!isEditing}
-                  disabled={!isEditing}
-                  {...methods.register("email", {
-                    onChange: handleChange,
-                    required: {
-                      value: true,
-                      message: "An email is required"
-                    },
-                    pattern: {
-                      value: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
-                      message: "You must enter a valid email (e.g. example@email.com)."
-                    }
-                  })}></input>
-                  <ErrorMessage
-                    errors={methods.formState.errors}
-                    name={"email"}
-                    render={({ messages }) => 
-                      messages &&
-                      Object.entries(messages).map(([type, message]) => (
-                        <p key={type} className='text-danger'>{message}</p>
-                      ))
-                    }
-                  />
-              </div>
-              <div className="container mb-3">
-                <label htmlFor="username" className="form-label">Username</label>
-                <input type="text" className="form-control" id="username" 
+              <EditInput 
+                value={passItem.email} 
+                label={"Email address"} 
+                type={"email"} 
+                id={"email"} 
+                pattern={/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/} 
+                isEditing={isEditing} 
+                errorMessage={"You must enter a valid email (e.g. example@email.com)."} 
+                maxLength={256}
+                customValidate={validateUsernameAndEmail} 
+                handleChange={handleChange}
+              />
+              <EditInput
                 value={passItem.username} 
-                readOnly={!isEditing}
-                disabled={!isEditing} 
-                {...methods.register("username", {
-                  onChange: handleChange,
-                  required: {
-                    value: true,
-                    message: "A username is required"
-                  },
-                  pattern: {
-                    value: /(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])/,
-                    message: "You must enter a username between 8 and 20 characters"
-                  }
-                })}></input>
-                <ErrorMessage
-                  errors={methods.formState.errors}
-                  name={"username"}
-                  render={({ messages }) => 
-                    messages &&
-                    Object.entries(messages).map(([type, message]) => (
-                      <p key={type} className='text-danger'>{message}</p>
-                    ))
-                  }
-                />
-              </div>
-              <div className='container mb-3'>
-                <label htmlFor="password" className="form-label">Password</label>
-                <div className="container-fluid input-group mb-3 px-0">
-                  <input type={passInputType} className="form-control" id="password" 
-                  value={passItem.password} 
-                  readOnly={!isEditing}
-                  disabled={!isEditing} 
-                  {...methods.register("password", {
-                    onChange: handleChange,
-                    required: {
-                      value: true,
-                      message: "A password is required"
-                    },
-                    maxLength: {
-                      value: 48,
-                      message: "Password must be no more than 48 characters."
-                    }
-                  })}></input>
-                  <ErrorMessage
-                    errors={methods.formState.errors}
-                    name={"password"}
-                    render={({ messages }) => 
-                      messages &&
-                      Object.entries(messages).map(([type, message]) => (
-                        <p key={type} className='text-danger'>{message}</p>
-                      ))
-                    }
-                  />
-                  <button type="button" className="btn btn-outline-secondary" onClick={handleShowPassword}>{showPassword}</button>
-                </div>
-              </div>
-              
-              <div className="container mb-3">
-                <label htmlFor="url" className="form-label">Website URL</label>
-                <input type="text" className="form-control" id="url" 
+                label={"Username"} 
+                type={"text"} 
+                id={"username"} 
+                pattern={/(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])/} 
+                isEditing={isEditing} 
+                errorMessage={"You must enter a username between 8 and 20 characters"} 
+                customValidate={validateUsernameAndEmail} 
+                maxLength={256}
+                handleChange={handleChange}
+              />
+              <EditPassInput
+                value={passItem.password} 
+                label={"Password"} 
+                type={passInputType} 
+                id={"password"} 
+                pattern={/()/} 
+                maxLength={48}
+                isEditing={isEditing}
+                showPassword={showPassword} 
+                errorMessage={"Password must be no more than 48 characters."} 
+                customValidate={() => methods.getValues("password") != ""} 
+                handleChange={handleChange}
+                toggleShowPassword={toggleShowPassword}
+              />
+              <EditInput
                 value={passItem.url} 
-                readOnly={!isEditing} 
-                disabled={!isEditing}
-                {...methods.register("url", {
-                  onChange: handleChange,
-                  required: {
-                    value: true,
-                    message: "A website URL is required"
-                  },
-                  pattern: {
-                    value: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-                    message: "You must enter a valid web address (e.g. https://www.google.com)"
-                  }
-                })}></input>
-                <ErrorMessage
-                  errors={methods.formState.errors}
-                  name={"username"}
-                  render={({ messages }) => 
-                    messages &&
-                    Object.entries(messages).map(([type, message]) => (
-                      <p key={type} className='text-danger'>{message}</p>
-                    ))
-                  }
-                />
-              </div>
+                label={"Website URL"} 
+                type={"text"} 
+                id={"url"} 
+                pattern={/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/} 
+                maxLength={48}
+                isEditing={isEditing} 
+                errorMessage={"You must enter a valid web address (e.g. https://www.google.com)"} 
+                customValidate={() => true}
+                handleChange={handleChange}
+              />
               <div className='container d-flex justify-content-center'>
                 <EditButton isEditing={isEditing} toggleEditPassword={toggleEditPassword} updatePassword={updatePassword}/>
                 <DeleteButton passItem={passItem} updatePassList={updatePassList} closeOffCanvas={handleClose}/>
               </div>
             </form>
-          </ErrorBoundary>
-        </Offcanvas.Body>
-      </>
+          </FormProvider>
+        </ErrorBoundary>
+      </Offcanvas.Body>
+    </>
     )
+}
+
+interface IEditInputProps {
+  value: string,
+  label: string,
+  type: string,
+  id: string,
+  pattern: RegExp,
+  isEditing: boolean,
+  errorMessage: string,
+  maxLength: number,
+  customValidate: () => boolean | string,
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+}
+
+interface EditPassInputProps {
+  value: string,
+  label: string,
+  type: string,
+  id: string,
+  pattern: RegExp,
+  isEditing: boolean,
+  errorMessage: string,
+  maxLength: number,
+  showPassword:string,
+  customValidate: () => boolean | string,
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+  toggleShowPassword: () => void,
+}
+
+function EditPassInput({ value, label, type, id, pattern, isEditing, errorMessage, maxLength, showPassword, customValidate, handleChange, toggleShowPassword }: EditPassInputProps) {
+  const { register, formState: {errors} } = useFormContext();
+
+  return (
+    <div className="container mb-3">
+      <div className='container'>
+        <label htmlFor={id} className="form-label">{label}</label>
+      </div>
+      <div className="container-fluid input-group mb-3 px-0">
+        <input type={type} className="form-control" 
+          id={id}  
+          value={value}
+          readOnly={!isEditing}
+          disabled={!isEditing}
+          {...register(id, {
+            onChange: handleChange,
+            validate: () => customValidate(),
+            pattern: {
+              value: pattern,
+              message: errorMessage
+            },
+            maxLength: {
+              value: maxLength,
+              message: `${label} must be no more than ${maxLength} characters.`
+            }
+            })}
+        />
+        <button type="button" className="btn btn-outline-secondary" onClick={toggleShowPassword}>{showPassword}</button>
+      </div>
+      <ErrorMessage
+        errors={errors}
+        name={id}
+        render={({ messages }) => 
+          messages &&
+          Object.entries(messages).map(([type, message]) => (
+            <p key={type} className='text-danger'>{message}</p>
+          ))
+        }
+      />
+    </div>
+  );
+}
+
+function EditInput({ value, label, type, id, pattern, isEditing, errorMessage, maxLength, customValidate, handleChange }: IEditInputProps) {
+  const { register, formState: {errors} } = useFormContext();
+
+  return (
+    <div className="container mb-3">
+      <div className='container'>
+        <label htmlFor={id} className="form-label">{label}</label>
+      </div>
+      <input type={type} className="form-control" 
+        id={id}  
+        value={value}
+        readOnly={!isEditing}
+        disabled={!isEditing}
+        {...register(id, {
+          onChange: handleChange,
+          validate: () => customValidate(),
+          pattern: {
+            value: pattern,
+            message: errorMessage
+          },
+          maxLength: {
+            value: maxLength,
+            message: `${label} must be no more than ${maxLength} characters.`
+          }
+          })}
+      />
+      <ErrorMessage
+        errors={errors}
+        name={id}
+        render={({ messages }) => 
+          messages &&
+          Object.entries(messages).map(([type, message]) => (
+            <p key={type} className='text-danger'>{message}</p>
+          ))
+        }
+      />
+    </div>
+  );
 }
 
 interface IDeletePassword {
