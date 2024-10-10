eb1e5c9c3a61884c154ffab24981abb30d114c88
Samwise-B <sam.burcher99@gmail.com>
2024-10-11 00:23:25 +0100

    updated placeholders

--- client/src/PasswordCreator.tsx
+++ client/src/PasswordCreator.tsx
@@ -77,7 +77,7 @@ export function NewPasswordForm({updatePasswordList, handleClose, handleError}: 
             value={methods.watch("email") || ""}
             maxLength={256}
             isEditing={true}
-            placeholder="example@email.com"
+            placeholder="type your email..."
             pattern={/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/}
             errorMessage = "You must enter a valid email"
             customValidate={validateUsernameAndEmail}
@@ -91,7 +91,7 @@ export function NewPasswordForm({updatePasswordList, handleClose, handleError}: 
             value={methods.watch("url") || ""}
             maxLength={256}
             isEditing={true}
-            placeholder="https://www.placeholder.com"
+            placeholder="https://www.example.com"
             pattern={/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/}
             errorMessage = "You must enter a valid website URL"
             customValidate={() => true}
